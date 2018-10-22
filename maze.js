f85bdd4231308af19bbccff70b313810eec28b49
marcgrant21 <marcgrant10@gmail.com>
2018-10-22 09:24:48 -0400

    Exercise 2

--- maze.js
+++ maze.js
@@ -5,4 +5,22 @@ function main() {
    lefwal.addEventListener("mouseover",function(){
      this.className = "boundary youlose";
    });
+
+   let allboundary = document.querySelectorAll(".boundary");
+   	let data = -1;
+
+   	for(var w = 0; w < allboundary.length; w++){
+   		if(allboundary[w].className.indexOf("example") != -1){
+   			data = w;
+   		}else{
+   			allboundary[w].addEventListener("mouseover", function(){
+   			//	document.getElementById("status").textContent = "You lose!"
+   				for(var w = 0; w < allboundary.length; w++){
+   					if( w != data){
+   						allboundary[w].className = "boundary youlose";
+   					}
+   				}
+   			});
+   		}
+   	}
 }

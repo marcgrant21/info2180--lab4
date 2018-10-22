6aa995ffbf16d438720ed2db3cfc46ec05fcd8a2
marcgrant21 <marcgrant10@gmail.com>
2018-10-22 12:29:26 -0400

    Exercise 4

--- maze.js
+++ maze.js
@@ -5,7 +5,7 @@ function main() {
    lefwal.addEventListener("mouseover",function(){
      this.className = "boundary youlose";
    });
-
+   //Exercise 2
    let allboundary = document.querySelectorAll(".boundary");
    	let data = -1;
 
@@ -23,11 +23,30 @@ function main() {
    			});
    		}
    	}
-    var end = document.getElementById("end");
+    //Exercise 3
+    /*var end = document.getElementById("end");
 
     	end.addEventListener("mouseover", function(){
     		if (document.getElementsByClassName("boundary youlose").length == 0){
     			alert("You Win!!");
+    		}
+    	});*/
+
+      // Exercise 4
+      let restart = document.getElementById("start");
+    	let status = document.getElementById("status");
+
+    	restart.addEventListener("click", function(){
+    		var mazwal = document.querySelectorAll(".boundary");
+    		if (mazwal.length != 0){
+    			for(var w = 0; w < mazwal.length; w++){
+    				if(mazwal[w].className == "boundary example"){
+    					continue;
+    				}
+    				mazwal[w].className = "boundary";
+    			}
+
+    			status.textContent = "Move your mouse over the \"S\" to begin.";
     		}
     	});
 }
